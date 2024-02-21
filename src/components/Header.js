c656333b1de8673ea42e16d5f26923cfd3a8c374
Anurag Malviya <anuragmalviya62@gmail.com>
2024-02-21 16:04:12 +0530

    i was good

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <div>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRyUgC55IChqYYFxo-zBRe9FuN6qKWXFHHrQ&usqp=CAU" alt="hhhhhhhhhh" />
+            </div>
+        </div>
+    )
+}
+export default Header;
