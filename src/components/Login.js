e14075afa1ec6be7f763f6e481c4ec3f8d9e91ae
Anurag Malviya <anuragmalviya62@gmail.com>
2024-03-02 16:49:26 +0530

    SignIN/SignUp from

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import Header from "./Header";
 const Login = () => {
-    const [IsLogged,setIsLogged] = useState(true);
+    const [IsLogged, setIsLogged] = useState(true);
     const notregistered = () => {
-           if(IsLogged){
+        if (IsLogged) {
             setIsLogged(!IsLogged);
-           }
-           else
-           setIsLogged(!IsLogged);
+        }
+        else
+            setIsLogged(!IsLogged);
 
     }
     return (
@@ -18,12 +18,13 @@ const Login = () => {
                     <img src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="logo" />
                 </div>
             </div>
-            <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 opacity-80">
+            <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 opacity-80 rounded-lg ">
                 <h1 className="text-rose-50 font-bold ... px-20">{IsLogged ? "Sign In" : "Sign Up"}</h1>
-                <input type="text" className="py-2 m-4  w-full" placeholder="Email" />
-                <input type="password" className="py-2 m-4  w-full" placeholder="Password" />
-                <button className="py-2 m-4  left-10 bg-red-700 px-16 w-full">{IsLogged ? "Sign In" : "Sign Up"}</button>
-                <p className="text-rose-50 font-bold ... px-20" onClick={notregistered}>{IsLogged ? "New User" : "Already User"}</p>
+                {!IsLogged ? <input type="text" className="py-2 m-4  w-full rounded-lg  bg-black text-white border-2 border-indigo-600" placeholder="Name" /> : " "}
+                <input type="text" className="py-2 m-4  w-full rounded-lg  bg-black text-white border-2 border-indigo-600" placeholder="Email Address" />
+                <input type="password" className="py-2 m-4  w-full rounded-lg   bg-black text-white border-2 border-indigo-600" placeholder="Password" />
+                <button type="button" className="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ... py-2 m-4  left-10 bg-red-700 px-16 w-full rounded-lg bg-black text-white border-2 border-indigo-600 ">{IsLogged ? "Sign In" : "Sign Up"}</button>
+                <p className="text-rose-50 font-bold ... px-20 cursor-pointer" onClick={notregistered}>{IsLogged ? "New User" : "Already_User/_SignIn"}</p>
             </form>
         </div>
     )
